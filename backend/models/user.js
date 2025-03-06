b8ab5d1c06458b78915936cfc045aea369b2c1cb
Cipherr674 <71639314+Cipherr674@users.noreply.github.com>
2025-03-06 15:24:25 +0530

    updated the profile and user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,1 +1,31 @@
-changeed again
+const userSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    purchasedTemplates: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ResumeTemplate",
+    }],
+    isPremiumUser: {
+      type: Boolean,
+      default: false,
+    },
+  });
+  
