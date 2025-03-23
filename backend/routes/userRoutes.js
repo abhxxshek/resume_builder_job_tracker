1740e2878e9f6bc7a2d1e190cf7efc657f6f7492
AkhilZacharia <akhilzacharia35@gmail.com>
2025-03-24 01:13:25 +0530

    jobs

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 router.use(express.json());
 const axios = require("axios");
 
+// use skills for searching
 router.get("/jobs", async (req, res) => {
   try {
     const options = {
@@ -40,15 +41,24 @@ router.get("/jobdetails/:id", async (req, res) => {
     const options = {
         method: 'GET',
         url: 'https://linkedin-data-api.p.rapidapi.com/get-job-details',
-        params: {id: req.params.id},
+        params: {id:req.params.id},
         headers: {
             'x-rapidapi-key': process.env.RapidApiKey,
             'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com'
     }
     };
-    const response = await axios.request(options);
-    //   console.log(response.data);
-      res.status(200).json(response.data);
+    async function fetchData() {
+        try {
+            const response = await axios.request(options);
+            console.log(response.data.data);
+            res.status(200).json(response.data.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+    fetchData();
+      
   } catch (error) {
     console.error("Error fetching data from API:", error);
     res.status(500).send("Error fetching data from API");
