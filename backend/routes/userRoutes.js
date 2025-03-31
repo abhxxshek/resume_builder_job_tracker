83e48e16b6157ca004a2d8f150c61734a2ae80d0
AkhilZacharia <akhilzacharia35@gmail.com>
2025-03-31 12:16:41 +0530

    added /user/savedStyle

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -25,12 +25,7 @@ router.get("/jobs", async (req, res) => {
       console.log('Profile not found');
   } else {
     const userSkills =  profile.skills.map(item => item.skill);
-    if (userSkills.length > 0) {
     console.log('Profile found:',userSkills.join(','));
-    }else{
-      console.log('No skills found');
-      res.status(200).json('No skills in your profile');
-    }
     const options = {
       method: 'GET',
       url: 'https://linkedin-data-api.p.rapidapi.com/search-jobs-v2',
@@ -213,12 +208,11 @@ router.get('/payment-details',async(req,res)=>{
     res.status(500).json({ message: 'Error fetching payment details' });
   }
 })
-
 // style update 
 router.get('/savedStyle', async (req, res) => {
   try {
      const newStyle = await styleModel.findOne({templateName: 'Template13.jsx'});
-      // console.log(newStyle);   
+      console.log(newStyle);   
      res.status(200).json(newStyle);
   }catch (error) {
      console.error('Error adding notification:', error);
